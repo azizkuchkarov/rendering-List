24e15ac9ec679ead3e91dab54e2fb588dd2f283a
User <aziz_kuchkarov91@mail.ru>
2022-08-06 16:32:19 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Movies from './components/movies';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
